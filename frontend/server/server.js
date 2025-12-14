47bb5cb1d98d71d8e2f284c1aa87424917b8abe9
Fumbre <ernarovskiyvlad@gmail.com>
2025-12-14 19:35:25 +0100

    ejs

--- frontend/server/server.js
+++ frontend/server/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const expressLayouts = require('express-ejs-layouts');
 const path = require("path");
 
 const app = express();
@@ -7,6 +8,25 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // console.log(process.env.DB_USERNAME);
+
+const manifest = require(path.join(__dirname, '..', 'dist', 'manifest.json'));
+app.locals.assets = manifest;
+
+// view engine ejs
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+
+// static folder
+app.use(express.static('dist'));
+
+// middleware for engine as layout
+app.set('layout', 'layout');
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: "Retro2Cbot"
+  });
+});
 
 app.get('/robot', async (req, res) => {
   const robots = await (await fetch('http://localhost:8080/robots')).json();
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,17 +1,18 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
 
 module.exports = (env, argv) => {
   const isDev = argv.mode === "development";
 
   return {
-    entry: "./src/scripts/main.js",
+    entry: "./assets/scripts/main.js",
     mode: isDev ? "development" : "production",
 
     output: {
       path: path.resolve(__dirname, "dist"),
       filename: isDev ? "bundle.js" : "bundle.[contenthash].js",
+      publicPath: '/',
       clean: true,
     },
 
@@ -37,9 +38,7 @@ module.exports = (env, argv) => {
     },
 
     plugins: [
-      new HtmlWebpackPlugin({
-        template: "./src/index.html",
-      }),
+      new WebpackManifestPlugin(),
       !isDev &&
       new MiniCssExtractPlugin({
         filename: "style.[contenthash].css",
