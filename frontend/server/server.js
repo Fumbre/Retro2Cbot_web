9c14484abef2588c8a17bcc12a8af036e32fd620
Abyss <fumbre@MBP-Abyss.home>
2025-12-14 17:37:22 +0100

    get data

--- frontend/server/server.js
+++ frontend/server/server.js
@@ -1,5 +1,5 @@
-const express = require("express");
 require("dotenv").config();
+const express = require("express");
 const path = require("path");
 
 const app = express();
@@ -8,6 +8,24 @@ const PORT = process.env.PORT || 3000;
 
 // console.log(process.env.DB_USERNAME);
 
+app.get('/robot', async (req, res) => {
+  const robots = await (await fetch('http://localhost:8080/robots')).json();
+  console.log(robots);
+  return res.json(robots)
+})
+
+app.get('/robot/status/:id', async (req, res) => {
+  try {
+    let robots = await (await fetch('http://localhost:8080/robots')).json();
+
+    const test = robots.data.filter((item) => item.robotCode.toLowerCase() == req.params.id.toLowerCase())[0]
+    console.log(test)
+    return res.json(test)
+  } catch (e) {
+    return res.redirect('/')
+  }
+
+})
 
 
 app.use(express.static(path.join(__dirname, "../dist")));
@@ -16,19 +34,3 @@ app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
 
-function pop() {
-  function test() {
-    console.log('test')
-  }
-}
-
-function pop3() {
-
-  function test() {
-    console.log('test')
-  }
-  
-}
-
-pop3();
-pop();
--- frontend/src/scripts/main.js
+++ frontend/src/scripts/main.js
@@ -7,3 +7,9 @@ import "../style/main.sass";
 // const app = el("div", { class: "app" }, "Hello robot");
 // mount(document.body, app);
 
+async function getRobot() {
+  const robots = await fetch('/robot');
+  console.log(await robots.json());
+}
+
+await getRobot();
