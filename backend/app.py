6f310c3e412f2a3bc54ff6664b0ef1fcac8039ec
Sunny01 <whs3296116638@outlook.com>
2026-01-05 21:47:49 +0100

    update

--- backend/app.py
+++ backend/app.py
@@ -5,14 +5,15 @@ import os
 from weboscket import router as websocket
 from api import router as api
 
-# load value of configurations from .env file
-load_dotenv()
 
 app.include_router(api)
 app.include_router(websocket)
 
 
 if __name__ == "__main__":
+    import uvicorn
+    # load value of configurations from .env file
+    load_dotenv()
     SERVER_IP = os.environ.get("SERVER_IP")
     SERVER_PORT = int(os.environ.get("SERVER_PORT"))
     # Uvicorn is a async web service framework. it can support normal and async Http request and websockets
