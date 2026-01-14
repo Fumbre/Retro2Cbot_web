3825a122bfaa503a89102e28e0a288188b630447
Fumbre <ernarovskiyvlad@gmail.com>
2026-01-14 02:01:21 +0100

    start home done

--- frontend/assets/scripts/main.js
+++ frontend/assets/scripts/main.js
@@ -2,6 +2,13 @@ import "../style/normalize.scss";
 import "../style/global.sass";
 import "../style/header.sass";
 import "../style/main.sass";
+
+import group_front_page from '../img/group_front_page.jpg'
+import member_1 from '../img/members/member_1.jpg'
+import mem2 from '../img/members/member_2.jpg'
+import mem3 from '../img/members/member_3.jpg'
+import mem4 from '../img/members/member_4.jpg'
+
 import { nodeWebsocket } from "@websocket/websocket"
 import { createReflectiveGraphic, wsReflectiveData } from "@partials/robot/Reflective/reflective";
 
